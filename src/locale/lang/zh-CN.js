298c35da735e94caf717e391592b1d40c75ea8d7
137105719@qq.com <137105719@qq.com>
2020-04-10 22:37:25 +0800

    first commit

---
+++ src/locale/lang/zh-CN.js
@@ -0,0 +1,13 @@
+export default {
+  home: '首页',
+  login: '登录',
+  deployment: '自动部署',
+  log: '日志',
+  tables_page: '多功能表格',
+  markdown_page: 'Markdown编辑器',
+  editor_page: '富文本编辑器',
+  error_store_page: '错误收集',
+  error_logger_page: '错误日志',
+  query: '带参路由',
+  params: '动态路由'
+}
